6e51b4466f9e38ad0f9d43e7b845ec3daf54e910
habenz <haben.zaidt@gmail.com>
2025-01-19 00:38:04 -0500

    add title and send button stubs

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,9 @@ function App({ items }) {
   return (
     <div className="flex justify-center w-full">
       <div className="w-5/6 max-w-lg">
+        <h1 className="text-xl flex justify-center mb-2 pb-2 font-semibold border-b">
+          Leftover Inventory Counter
+        </h1>
         {items.map((item) => {
           if (item.types) {
             return (
@@ -20,6 +23,9 @@ function App({ items }) {
             return <ItemAdder name={item.name} key={item.name} />;
           }
         })}
+        <button className="w-full py-2 bg-black rounded text-slate-100">
+          Send Screenshot
+        </button>
       </div>
     </div>
   );
